55a298efd8c40c6f77b22d9c61baaf2af2974825
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-05-06 17:22:23 -0400

    Spring Bean Life Cycle Demo

--- src/main/java/com/prasant/spring/service/PrimaryGreetingServiceImpl.java
+++ src/main/java/com/prasant/spring/service/PrimaryGreetingServiceImpl.java
@@ -1,11 +1,5 @@
 package com.prasant.spring.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -13,31 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 @Primary
 @Profile({"en", "default"})
-public class PrimaryGreetingServiceImpl implements GreetingService, InitializingBean, DisposableBean {
+public class PrimaryGreetingServiceImpl implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
 		return "Hello from PrimaryGreetingServiceImpl.";
-	}
-	
-	@PostConstruct
-	public void postConstruct() {
-		System.out.println("In postConstruct() of PrimaryGreetingServiceImpl");
-	}
-	
-	@PreDestroy
-	public void preDestroy() {
-		System.out.println("In preDestroy() of PrimaryGreetingServiceImpl");
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		System.out.println("In afterPropertiesSet() of PrimaryGreetingServiceImpl");		
-	}
-	
-	@Override
-	public void destroy() throws Exception {
-		System.out.println("In destroy() of PrimaryGreetingServiceImpl");		
 	}
 	
 }
